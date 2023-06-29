5206dd40538bb3790a7066030614e99072b0d52f
Yoyochou0901 <yoyochou0901@gmail.com>
2023-06-29 16:15:55 +0800

    新增使用測站代碼功能

--- GAP_cal.py
+++ GAP_cal.py
@@ -1,4 +1,5 @@
 import math
+from stalist import *
 
 def sta_epi_angle(station):
     #計算震央及測站間的角度
@@ -19,9 +20,24 @@ epicenter = (epicenter_longitude, epicenter_latitude)
 
 #各測站座標
 stations = []
+
+ans = input("輸入測站資訊時使用代碼(1)或座標(2)?:")
+if ans == "1":
+    print("使用代碼")
+else:
+    print("使用座標")
 for i in range(num_stations):
-    longitude = float(input("測站 {} 經度: ".format(i+1)))
-    latitude = float(input("測站 {} 緯度: ".format(i+1)))
+    longitude = ""
+    if ans == "1":
+        sta_code = input("測站 {} 代碼: ".format(i+1))
+        if sta_code not in sta_list:
+            print("未找到測站，請輸入測站座標")
+        else:
+            longitude = round(sta_list[sta_code]["Long"],2)
+            latitude = round(sta_list[sta_code]["Lat"],2)
+    elif longitude == "":
+        longitude = float(input("測站 {} 經度: ".format(i+1)))
+        latitude = float(input("測站 {} 緯度: ".format(i+1)))
     station = (longitude, latitude)
     sta_angle = sta_epi_angle(station)
     stations.append((sta_angle))
@@ -40,6 +56,6 @@ for i in range(num_stations):
 if num_stations == 2:
     #GAP明顯地震可只輸入2站資料
     print("\nGAP1:", round(max_angle,2))
-    print("GAP2:", 360-round(max_angle,2))
+    print("GAP2:", round(360-max_angle,2))
 else:
     print("\nGAP:", round(max_angle,2))
